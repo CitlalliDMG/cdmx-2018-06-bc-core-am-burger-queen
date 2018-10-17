ef1915b2c44913966222230906be6c350a85932f
Citlalli DMG <citlallidmg@gmail.com>
2018-10-16 20:23:59 -0500

    Updates

---
+++ precache-manifest.d41c0ad636a0f094b1a1a5c0124c0981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "977850d7cb0bf372136d",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.977850d7.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "977850d7cb0bf372136d",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.8e8e9d35.chunk.css"
+  },
+  {
+    "revision": "952c98893c4af6149ef4a8454888499f",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
